713b6e90b1da813b1a73b3f77dc784539b0b4ff9
Steven <77511759+guoyusteven@users.noreply.github.com>
2024-11-03 15:57:57 +0000

    Add files via upload

--- zhuyin.js
+++ zhuyin.js
@@ -1,15 +1,23 @@
 var zhuyinAdded = false;
 
-chrome.browserAction.onClicked.addListener(function(tab) {
-
-	if (!zhuyinAdded) {
-		chrome.tabs.insertCSS(null, {file: "zhuyin.css"});
-		zhuyinAdded = true;
-	} else {
-		chrome.tabs.insertCSS({
-			code: '* {font-family: null !important;}'
-		});
-		zhuyinAdded = false;
-	}
-
+browser.browserAction.onClicked.addListener(function(tab) {
+    if (!zhuyinAdded) {
+        browser.tabs.insertCSS(null, {file: "zhuyin.css"})
+            .then(() => {
+                zhuyinAdded = true;
+            })
+            .catch((error) => {
+                console.error("Error inserting CSS: ", error);
+            });
+    } else {
+        browser.tabs.insertCSS({
+            code: '* {font-family: null !important;}'
+        })
+        .then(() => {
+            zhuyinAdded = false;
+        })
+        .catch((error) => {
+            console.error("Error removing CSS: ", error);
+        });
+    }
 });
